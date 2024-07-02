2d155b85e306a73ca74e525c4d25800f097a1d35
winatabayu <winatabayu01@gmail.com>
2024-07-02 11:38:54 +0700

    [CHG] add db transaction on action

--- src/Abstracts/BaseAction.php
+++ src/Abstracts/BaseAction.php
@@ -2,14 +2,30 @@
 
 namespace Winata\PackageBased\Abstracts;
 
+use Illuminate\Support\Facades\DB;
+
 abstract class BaseAction
 {
+    public bool $usingDBTransaction = true;
     public function __construct()
     {
-        return $this->rules();
+        $rules = $this->rules();
+
+        if ($this->usingDBTransaction){
+            DB::beginTransaction();
+        }
+
+        return $rules;
     }
 
     public abstract function rules(): self;
 
     public abstract function handle(): mixed;
+
+    public function __destruct()
+    {
+        if ($this->usingDBTransaction){
+            DB::commit();
+        }
+    }
 }
